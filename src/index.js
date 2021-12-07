9109cea2cfe34c2bbca1932cb4b46ccbb9534145
Jonas Rodrigues <F5115299@bb.com.br>
2021-12-07 00:53:02 -0300

    desafio2

--- src/index.js
+++ src/index.js
@@ -10,19 +10,70 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+  
+  if(!user){
+    return response.status(404).json({ error: "Usuario não existe"});
+  }
+  
+  request.user = user;
+  
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+
+  const { user } = request;
+
+  if(user.pro)
+    return next();
+  
+  if((user.todos.length)<10){
+    return next();
+  }else{
+    return response.status(403).json({ error: "Plano Free expirado, ative o plano Pro"});
+  }
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+ 
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user){
+    return response.status(404).json({ error: "Usuário não existe"});
+  }
+
+
+  if(!validate(id))
+    return response.status(400).json({ error: "TodoID não localizado"});
+
+  const todo = user.todos.find(todo => todo.id === id);
+  
+  if(!todo)
+    return response.status(404).json({ error: "Todo não localizado"});
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if(!user)
+    return response.status(404).json({ error: "Usuário não localizado"});
+
+  request.user = user;
+  
+  return next();
 }
 
 app.post('/users', (request, response) => {
